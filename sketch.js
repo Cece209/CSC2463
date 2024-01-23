a3ffe600050fcd8e49383d5f0594622ab98bb12a
Cece209 <143728721+Cece209@users.noreply.github.com>
2024-01-23 16:29:39 -0600

    Assigment 1

--- sketch.js
+++ sketch.js
@@ -1,11 +1,31 @@
 function setup() {
-  createCanvas(500, 250);
+  createCanvas(1000, 1000);
 }
 
 function draw() {
-  background(0, 255, 0);
-  circle(125, 120, 210)
-  square(285, 25, 190)
+  background(225);
+  fill(0, 255, 0);
+  rect(10, 10, 290, 150);
+ 
+  fill(255);
+  circle(80, 85, 120);
+
+  fill(255);
+  square(160, 25, 120);
+
+  fill(255);
+  square(10, 200, 300);
+
+  fill(255, 0, 0, 100);
+  circle(160, 300, 125);
+
+  fill(0, 0, 255, 100);
+  circle(110, 370, 125);
+
+  fill(0, 255, 0, 100)
+  circle(210, 370, 125)
+
+  
 }
 
 
